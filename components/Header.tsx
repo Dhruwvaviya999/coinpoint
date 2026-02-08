2174be5da5bf9bde0f507156352af7816ec8d415
Dhruwvaviya999 <dhruwvaviya123@gmail.com>
2026-02-08 11:32:42 +0530

    implemented coin overview and trending coins ui and functionality using the fetcher utility fucntion

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
     <header>
         <div className='main-container inner'>
             <Link href="/">
-                <Image src="/assets/logo.svg" alt='CointPoint logo' width={132} height={40} />
+                <Image src="/logo.svg" alt='CointPoint logo' width={132} height={40} />
             </Link>
             <nav>
               <Link href="/" className={cn('nav-link', {
